58a131a4a760d2f87955bb4b8eee007b0ab7071f
Ivan <babkov900@gmail.com>
2023-09-18 15:32:58 +0300

    added react-router and react-bootstrap

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+// import Home from './Home'; // Замените на ваши компоненты
+// import About from './About';
+// import Contact from './Contact';
+
+// const Routes = () => {
+//   return (
+//     <Switch>
+//       <Route exact path="/" component={Home} />
+//       <Route path="/about" component={About} />
+//       <Route path="/contact" component={Contact} />
+//     </Switch>
+//   );
+// }
+
+// export default Routes;
+ 
