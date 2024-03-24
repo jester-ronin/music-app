95a461b44dd07f41c52a0681db57cd260c0c9f8e
jester-ronin <82144563+jester-ronin@users.noreply.github.com>
2024-03-24 16:07:33 +0200

    gihub-pages fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from "./Redusers/store";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 
 
@@ -18,9 +18,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
       <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
