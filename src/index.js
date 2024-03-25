7b7e95d6f13f237d5c5558ea34a42c5ec23f5684
jester-ronin <82144563+jester-ronin@users.noreply.github.com>
2024-03-25 15:29:23 +0200

    HashRouter fix

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from "./Redusers/store";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from "react-router-dom"
-
-
-
-
-
-
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <HashRouter>
-      <App />
+        <App />
       </HashRouter>
     </Provider>
   </React.StrictMode>
